de7052cd2e5d7c83ee71179e3c43a48dd848a63e
Rafael Costa <rafaelcosta8020@gmail.com>
2023-12-31 16:09:33 -0300

    first migration

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using rpg.Models;
+
+namespace rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using rpg.Data;
 using rpg.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+);
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
